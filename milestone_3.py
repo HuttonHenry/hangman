e7713f476cb920d5ba6fa6fc0052fc052f6e6d7e
HuttonHenry <82598725+HuttonHenry@users.noreply.github.com>
2023-01-16 21:54:27 +0000

    Added more validation.

--- milestone_3.py
+++ milestone_3.py
@@ -2,13 +2,23 @@ import random
 word_list=['oranges','melon','banana','apples','cherries']
 word = random.choice(word_list)
 
-while True:
-    guess = input("Guess a letter: ")
-    if guess.isalpha() and len(guess) == 1:
-        break
-    print("Invalid letter. Please, enter a single alphabetical character.")
+def ask_for_input():
+    while True:
+        guess = input("Guess a letter: ").lower()
+        if guess.isalpha() and len(guess) == 1:
+            break
+        print("Invalid letter. Please, enter a single alphabetical character.")
+    
+    return guess
 
-if guess in word:
-    print("Good guess! ", guess, " is in the word.")
-else:
-    print("Sorry, ", guess, " is NOT in the word.")
+
+def check_guess(guess2):
+    print(guess2)
+    print(word)
+    if guess2 in word:
+        print("Good guess! ", guess2, " is in the word.")
+    else:
+        print("Sorry, ", guess2, " is NOT in the word.")
+
+myguess = ask_for_input()
+check_guess(myguess)
