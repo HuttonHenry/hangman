6f7f034e3f2fc2811a75f0dbe919783b5e992c77
HuttonHenry <82598725+HuttonHenry@users.noreply.github.com>
2023-01-16 21:41:09 +0000

    Checks the input against the random word.

--- milestone_3.py
+++ milestone_3.py
@@ -1,5 +1,14 @@
+import random
+word_list=['oranges','melon','banana','apples','cherries']
+word = random.choice(word_list)
+
 while True:
     guess = input("Guess a letter: ")
     if guess.isalpha() and len(guess) == 1:
         break
     print("Invalid letter. Please, enter a single alphabetical character.")
+
+if guess in word:
+    print("Good guess! ", guess, " is in the word.")
+else:
+    print("Sorry, ", guess, " is NOT in the word.")
