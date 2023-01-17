cbb9d2e501ec4eea09825ccb82972c2474d386b6
HuttonHenry <82598725+HuttonHenry@users.noreply.github.com>
2023-01-17 20:48:41 +0000

    address stuff

--- milestone_4.py
+++ milestone_4.py
@@ -3,28 +3,24 @@ import random
 class Hangman:
     word_list=['oranges','melon','banana','apples','cherries']
 
-    def __init__(self,word_list,num_lives):
+    def __init__(self):
         self.num_lives = 5
-        self.word = random.choice(word_list)
+        self.word = random.choice(self.word_list)
         self.wordguessed = []
         self.num_letters = 0
         self.list_of_guesses = []
 
-    def ask_for_input():
+    def ask_for_input(self):
         while True:
             guess = input("Guess a letter: ").lower()
             if guess.isalpha() and len(guess) == 1:
                 break
             print("Invalid letter. Please, enter a single alphabetical character.")
-        return guess
 
-
-    def check_guess(guess2):
-        if guess2 in word:
-            print("Good guess! ", guess2, " is in the word.")
+        if guess in self.word:
+            print("Good guess! ", guess, " is in the word.")
         else:
-            print("Sorry, ", guess2, " is NOT in the word.")
+            print("Sorry, ", guess, " is NOT in the word.")
     pass
 
-myguess = Hangman.ask_for_input()
-Hangman.check_guess(myguess)
+myguess = Hangman().ask_for_input()
