18d469cc6e49a96807c0e9498fcefa4d4b4364f6
HuttonHenry <82598725+HuttonHenry@users.noreply.github.com>
2023-01-18 20:08:18 +0000

    Fixes word_guesed.

--- milestone_4.py
+++ milestone_4.py
@@ -5,13 +5,12 @@ class Hangman:
 
     def __init__(self,word_list,num_lives=5):
         self.word = random.choice(word_list)
-        self.wordguessed = ["_"] * len(self.word)
+        self.word_guessed = ["_"] * len(self.word)
         self.unique_letters = set(self.word)
         self.num_letters = 0
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
-
 
     def ask_for_input(self):
         while True:
