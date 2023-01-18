a50c6a737fc4457f01c1c350d152e108a642efe0
HuttonHenry <82598725+HuttonHenry@users.noreply.github.com>
2023-01-18 20:10:24 +0000

    Fixes num_letters

--- milestone_4.py
+++ milestone_4.py
@@ -7,7 +7,7 @@ class Hangman:
         self.word = random.choice(word_list)
         self.word_guessed = ["_"] * len(self.word)
         self.unique_letters = set(self.word)
-        self.num_letters = 0
+        self.num_letters = len(set(self.word))
         self.num_lives = num_lives
         self.word_list = word_list
         self.list_of_guesses = []
