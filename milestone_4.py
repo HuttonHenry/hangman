32d7f86f738a3bca52fa7911d8feca9569a7042c
HuttonHenry <82598725+HuttonHenry@users.noreply.github.com>
2023-01-19 18:57:10 +0000

    remove that!

--- milestone_4.py
+++ milestone_4.py
@@ -14,10 +14,7 @@ class Hangman:
         guess = guess.lower()
         if guess in self.word:
             print(f"Good guess! {guess} is in the word.")
-            for letter in self.word:
-                if guess == letter:
-                    self.word_guessed.index = letter
-        self.num_letters = self.num_letters -1 
+
 
     def ask_for_input(self):
         while True:
